d27476e80f556cdafc1868cd126fe4cece9e37d5
Oliul Alam <67019458+oliulalam@users.noreply.github.com>
2022-03-25 00:14:02 +0600

    Library functions for string

---
+++ 8-Library functions for string/LibraryFunction.js
@@ -0,0 +1,37 @@
+// library function mane hocche emon sokol function je gula age hote banano ache amara se gula just use korbo.
+
+
+//string er length jante var er nam er pore .length use korte hoy
+var text = "bangladesh";
+console.log("Number of length: " + text.length);
+
+
+// user er kache hote input nite (prompt) eta use korte hoy. 
+// var text1 = prompt("Enter your name: ");
+// document.write(text1);
+
+
+//string er majhe kono ekta latter ke dekhte use kore hoy (varName.charAt(4))
+var text2 = "Bangladesh";
+document.write(text2.charAt(3));
+
+
+//upper case lower case a string ke convate korte use kota hoy
+var text3 = "Bangladesh ";
+document.write(text3.toLowerCase());
+
+var text4 = "Bangladesh ";
+document.write(text4.toUpperCase());
+
+//string concatenation korte (varName.concat()); use korte hoy
+
+var text5 = "Bangladesh";
+var text6 = " Is beautyfull country ";
+
+document.write(text5.concat(text6));
+
+
+//text ke slice korte (varName.slice()); er majhe index number koto theke koto pojonto slice korte chan ta dite hobe.
+
+var text7 = "bangladesh";
+document.write(text7.slice(1,5));

d85a95173d2f4a8a3065cf26fe552efd9ae06bb4
Oliul Alam <67019458+oliulalam@users.noreply.github.com>
2022-03-24 22:04:51 +0600

    class 6 done

---
+++ 6- number method  toFixed  toPrecision/index.js
@@ -0,0 +1,13 @@
+// var num = 20;
+//number theke string a convate korte bebohar kora hoy toString 
+// num =toString(num);
+// console.log(typeof(num)); 
+
+// var num = "20";
+// // stiring theke number a convart korte use kora hoy
+// num =parseInt(num);
+// console.log(typeof(num)); 
+
+var num = 20.5664545;
+//tofixd er maddhome .er pore koyta sonkha ache ta bole dite pari
+console.log(num.toFixed(3)); 

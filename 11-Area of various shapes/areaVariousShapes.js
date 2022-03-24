e74509c2d2ac1847891e5580bcbd1c7d45af6f39
Oliul Alam <67019458+oliulalam@users.noreply.github.com>
2022-03-25 02:55:17 +0600

    Area of various shapes

---
+++ 11-Area of various shapes/areaVariousShapes.js
@@ -0,0 +1,29 @@
+//Square = Base * Hight
+// Rectangle = Base * Hight
+// Triangle = Base * perpendicular Height 
+// trapezium = (a+b)* height 2
+// parallelogram = Base * perpendicular height
+//Rombus = Lenght * Height
+// kite = Lenght * Height 
+
+// var base = parseFloat(prompt("Enter Base = "));
+// var height = parseFloat(prompt("Enter Height = "));
+
+// var area = base * height;
+// document.write("Area is : ",area ,"<br/>");
+
+
+// var tringle = (base * height)*2;
+// document.write("Tringle is : ",tringle);
+
+
+var a = parseFloat(prompt("Enter a value : "));
+var b = parseFloat(prompt("Enter a value : "));
+
+var height = parseFloat(prompt("Enter Height : "));
+
+var trapezium = (a + b) * height;
+document.write("trapezium is : ",trapezium);
+
+
+

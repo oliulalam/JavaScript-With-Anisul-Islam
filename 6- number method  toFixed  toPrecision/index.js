7aabb3695d0d483b232e43467e73e408a760c38e
Oliul Alam <67019458+oliulalam@users.noreply.github.com>
2022-03-24 22:35:17 +0600

    number method toFixed toPrecision

--- 6- number method  toFixed  toPrecision/index.js
+++ 6- number method  toFixed  toPrecision/index.js
@@ -1,13 +1,29 @@
-// var num = 20;
+var num1 = 20;
 //number theke string a convate korte bebohar kora hoy toString 
-// num =toString(num);
-// console.log(typeof(num)); 
+num1 =toString(num1);
+console.log(typeof(num1)); 
 
-// var num = "20";
-// // stiring theke number a convart korte use kora hoy
-// num =parseInt(num);
-// console.log(typeof(num)); 
+var num2 = "20";
+// stiring theke number a convart korte use kora hoy
+num2 =parseInt(num2);
+console.log(typeof(num2)); 
 
-var num = 20.5664545;
+var num3 = 20.5664545;
 //tofixd er maddhome .er pore koyta sonkha ache ta bole dite pari
-console.log(num.toFixed(3)); 
+console.log(num3.toFixed(3)); 
+
+var number1 = 20.777837;
+// total kota sonkkha show korate chan ta set kore  .toPrecision
+console.log(number1.toPrecision(3));
+
+// console log (Number("12")) use korle string ta ke number a convart kore nibe  
+console.log(Number("12"));
+
+//amar chaile type tao dekhte pari typeof use kore 
+console.log(typeof(Number("12")));
+
+// true ke jodi number a convart korte chai tahole print korte 1
+console.log(Number(true));
+
+// false ke jodi number a convart korte chai tahole print korte 0
+console.log(Number(false));

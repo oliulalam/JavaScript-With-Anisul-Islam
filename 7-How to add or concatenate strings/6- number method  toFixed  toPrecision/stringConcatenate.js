acc65edb544cfb745dce521a91041ae2595333f2
Oliul Alam <67019458+oliulalam@users.noreply.github.com>
2022-03-24 23:03:02 +0600

    how to add or concatenate strings

---
+++ 7-How to add or concatenate strings/6- number method  toFixed  toPrecision/stringConcatenate.js
@@ -0,0 +1,20 @@
+// 2 ba odhik string er majhe  + operator use kore 2 ta string ke add kore  string concatenation bole
+document.write("Oliul" + " Alam<br/>")
+//Nicher ei vabeo string concatanation kora jay
+// var firstName = "Oliul";
+// var secondName ="Alam ";
+
+// var fullName = firstName + secondName;
+// document.write("My name is : " + fullName);
+
+
+// ei vabeo kora jay 
+
+// var fullName1 = "Oliul Alam";
+
+// document.write(fullName1 + " Is My Name");
+
+
+var fullName1 = "Oliul Alam";
+
+document.write("My Name Is " + fullName1 + " Joy");

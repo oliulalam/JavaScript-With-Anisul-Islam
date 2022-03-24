500ea045e8152cbf8331ff0cd319293762fcc972
Oliul Alam <67019458+oliulalam@users.noreply.github.com>
2022-03-25 01:02:16 +0600

    Arithmetic and assignment operator

---
+++ 9-Arithmetic and assignment operator/ArithmeticAssiment.js
@@ -0,0 +1,76 @@
+//Arithmetic operator -> +, -, *, /, %(Modulaus), ** (Exponent) , ++, --;
+ 
+// + jog
+// var num1 = 20;
+// var num2 = 30;
+// sum = num1 + num2;
+// document.write(sum);
+
+// - biyog
+// var num1 = 20;
+// var num2 = 30;
+// sum = num1 - num2;
+// document.write(sum);
+
+// * gun
+// var num1 = 20;
+// var num2 = 30;
+// sum = num1 * num2;
+// document.write(sum);
+
+// / vag
+// var num1 = 20;
+// var num2 = 30;
+// sum = num1 / num2;
+// document.write(sum);
+ 
+// % Modulas
+// var num1 = 20;
+// var num2 = 30;
+// sum = num1 % num2;
+// document.write(sum);
+
+// ++ 1 kore barabe
+// var num1 = 20;
+// num1++;
+// document.write(num1);
+
+// -- 1 mainaj korbe 
+// var num2 = 20;
+// num2--;
+// document.write(num2);
+
+// Assignment operator -> =, +=, -=, *=, /=, %=, **=
+// += 
+// var  x = 3;
+// x += 6; // x = x + 6.
+// document.write(x);
+
+// -= 
+// var y = 3;
+// y -=6;
+// document.write(y);
+
+
+// *=
+// var x = 3;
+// x *= 6;
+// document.write(x);
+
+// /=
+// var x = 3;
+// x /= 6;
+// document.write(x);
+
+// %=
+// var x = 3;
+// x %= 6;
+// document.write(x);
+
+// **=
+var x = 3;
+x **= 6;
+document.write(x);
+
+
+

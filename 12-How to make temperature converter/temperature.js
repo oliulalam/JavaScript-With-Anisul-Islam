350d1d3aa9966c1fe48ea2b04eb4c6266ce1de09
Oliul Alam <67019458+oliulalam@users.noreply.github.com>
2022-03-25 12:20:30 +0600

    how to make temperature converter

---
+++ 12-How to make temperature converter/temperature.js
@@ -0,0 +1,15 @@
+//Fahrenheit to Celsius  a convart korte use kora hoy. formula (farn-32) *(5/9);
+
+
+// var farn = prompt("Enater farn: ");
+
+// var cels =(farn-32) *(5/9);
+// document.write("Celsius = ", cels.toFixed(1));
+
+
+//Celsius to Fahrenheit convart korte use kor hoy . formula (12°C × 9/5) + 32 = 53.6°F
+
+var cels = prompt("Enater fahrenhrit : ")
+
+var farn =(cels * (9/5)) + 32;
+document.write("Fahrenhrit = ", farn.toFixed(1));

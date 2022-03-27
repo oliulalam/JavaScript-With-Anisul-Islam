d6bce4f4e1ca04545fd542032c326a0511b44ee3
Oliul Alam <67019458+oliulalam@users.noreply.github.com>
2022-03-28 00:18:10 +0600

    Relational and Logical Operator

---
+++ 13-Relational and Logical Operator/RelationalLogiscalOp.js
@@ -0,0 +1,32 @@
+// Relational operator -> >, >=, <, <=, ==, ===, !=, !==
+
+// var num1 = 30;
+// var num2 = 20;
+
+// console.log(num1>num2);
+
+// var num1 = 30;
+// var num2 = 20;
+
+// console.log(num1<num2);
+
+
+//var num1 = 20;
+//var num2 = 30;
+
+//console.log(num1==num2);
+
+// var num1 = 20;
+// var num2 = 30;
+
+// console.log(num1!=num2);
+
+
+//Logical operator -> &&, ||, 
+
+var num1 = 40;
+var num2 = 30;
+
+//console.log(num1>num2 && num1>num2)
+
+console.log(num1>num2 || num1>num2)
